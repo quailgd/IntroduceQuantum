617994b0ce8ac7a0d30aefd8bdc10ae7185800e9
gdquail <24057@sshs.hs.kr>
2026-05-28 14:29:53 +0900

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 function App() {
-  const [page, setPage] = useState(1);
+//  const [page, setPage] = useState(1);
   return (
     <>
       <h1>ㅎㅇㅎㅇ</h1>
